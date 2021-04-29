4e99be7a2b45f8a40f052de3a80015c55bc351c2
gezongpan <gezongpan@58.com>
2021-04-29 11:05:23 +0800

    增加TextView

--- app/src/main/java/com/example/studyApp/customView/test/TestActivity.kt
+++ app/src/main/java/com/example/studyApp/customView/test/TestActivity.kt
@@ -1,12 +1,28 @@
 package com.example.studyApp.customView.test
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.studyApp.R
+import kotlinx.android.synthetic.main.activity_test.*
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
+        Handler().postDelayed({
+            startViewAnimation()
+        }, 1000)
+    }
+
+    private fun startViewAnimation() {
+        ObjectAnimator
+                .ofFloat(gradient_text, "mPercent", 0f, 1f)
+                .apply {
+                    duration = 3000
+                }
+                .start()
     }
 }
